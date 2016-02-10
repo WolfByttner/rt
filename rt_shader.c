681d7c1eea3ce085bb0724ef5b4060e4ac4ddb6e
Frederic NIETO <fnieto@e3r3p13.42.fr>
2016-02-10 18:52:26 +0100

    corrected depth

--- rt_shader.c
+++ rt_shader.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 00:00:53 by fnieto            #+#    #+#             */
-/*   Updated: 2016/02/10 16:34:42 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/02/10 18:49:47 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ typedef	struct		s_ret
 	t_geo		object;
 }					t_ret;
 
-__constant			PI = 3.14159265358979323846;
+__constant double	PI = 3.14159265358979323846;
 
 static int			encode(double3 col)
 {
@@ -60,13 +60,26 @@ static t_ret		sphere_dst(t_cam cam, t_geo sp, t_ret prev)
 	double st = step(0.0, min(t,d));
 	double m = mix(-1.0, t, st);
 	ret.t = m;
-	if ((ret.t > 0 && prev.t <= 0) || (prev.t > 0 && ret.t < prev.t))
+	if ((ret.t > 0 && prev.t <= 0) || (ret.t > 0 && prev.t > 0 && ret.t < prev.t))
 	{
 		ret.object = sp;
 		return (ret);
 	}
 	ret = prev;
 	return (ret);
+}
+
+static t_ret		raytrace(t_cam cam, t_geo *geoms, size_t size)
+{
+	size_t		i;
+	t_ret		tmp;
+
+	tmp.t = -1;
+	i = -1;
+	while (++i < size)
+		if (geoms[i].type == 0)
+			tmp = sphere_dst(cam, geoms[i], tmp);
+	return (tmp);
 }
 
 static double3		rotate_vec(double3 vec, double3 angls)
@@ -109,21 +122,19 @@ __kernel void		shader(
 	double2				coord;
 	double2				uv;
 	t_cam				cam;
-	t_geo				sphere;
+	t_geo				spheres[] = {{0, {0, 0, 0}, {1, 0, 0}, {0, 0, 0}}, {0, {0, 2, 2}, {1, 0, 0}, {0, 0, 0}}, {0, {0, 0, 10}, {7, 0, 0}, {0, 0, 0}}};
 
 	id = get_global_id(0);
 	if (((size_t)id) >= count)
 		return ;
 	coord = (double2)((id % (int)(res.x)), id / ((int)(res.x)));
-	uv = ((coord / res) - 0.5) * (double2)(1, -1) * normalize(res) * zoom
-		* PI / 2 + rot * PI / 50;
-	cam.ray = rotate_vec((double3)(0, 0, 1), (double3)(uv.x, 0, uv.y));
-	cam.pos = pos.zxy * (double3)(0.1, 10, 10) + (double3)(0, 0, -3);
-	sphere.pos = (double3)(0, 0, 0);
-	sphere.dim.x = 1;
+	uv = -((coord / res) - 0.5) * (double2)(1, -1) * normalize(res) * zoom
+		* PI / 2 - rot * PI / 50;
+	cam.ray = normalize(rotate_vec((double3)(0, 0, 1), (double3)(uv.x, 0, uv.y)));
+	cam.pos = pos.xzy * (double3)(10, 0.1, 10) + (double3)(0, 0, -3);
 	t_ret tmp;
-	tmp.t = -1;
-	double t = sphere_dst(cam, sphere, tmp).t;
-	output[id] = encode((double3)(1 / t));
+	tmp = raytrace(cam, spheres, sizeof(spheres) / sizeof(t_geo));
+	output[id] = encode((double3)(1 / tmp.t));
+//	output[id] = *(long*)&t;
 }
 
