8d660cb0bce32e55212631bfe7e7ea3cb32f06d6
Johan BYTTNER <jbyttner@student.42.fr>
2016-03-22 16:45:50 +0100

    Added load-file utility

---
+++ OpenGl/load_file.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_file.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/22 16:04:19 by jbyttner          #+#    #+#             */
+/*   Updated: 2016/03/22 16:42:11 by jbyttner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include "rt.h"
+
+void	load_file(char *file, GLchar **ptr, GLint *length)
+{
+	int		r;
+	int		fd;
+	char	buf[BUFF_LENGTH];
+	char	*res;
+
+	*length = 0;
+	res = NULL;
+	if ((fd = open(file, O_RDONLY)) == -1)
+		err("Cannot open file!");
+	while ((r = read(fd, buf, BUFF_LENGTH)) > 0)
+	{
+		if (!(res = realloc(res, *length + r)))
+			err("No memory!");
+		memcpy(res + *length, buf, r);
+		*length += r;
+	}
+	if (r == -1)
+		err("Failed reading file!");
+	*ptr = res;
+}
--- OpenGl/rt.h
+++ OpenGl/rt.h
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 19:50:48 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/22 12:25:30 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/03/22 16:43:07 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 # define RT_H
 # include <GLFW/glfw3.h>
 # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft.h"
 # define P_NAME "foobar"
+# define BUFF_LENGTH 100
 
 void				err(char *msg);
+void				load_file(char *file, GLchar **ptr, GLint *length);
 GLuint				shader(GLenum t, GLsizei c, GLchar **s, GLint *l);
 GLuint				shader_program(GLuint *shaders, size_t size);
 
