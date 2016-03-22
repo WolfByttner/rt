54602dd3b31e2f85bdf1c1a69f436b1cf0d6d1be
Frederic NIETO <fnieto@e1r11p7.42.fr>
2016-03-22 17:03:16 +0100

    made vbo/vaos

---
+++ OpenGl/vao.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vao.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/22 16:06:05 by fnieto            #+#    #+#             */
+/*   Updated: 2016/03/22 17:01:52 by fnieto           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+GLuint		vao(void)
+{
+	return (glGenVertexArrays());
+}
+
+void		vao_add_indices(GLuint vao, GLuint vbo)
+{
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
+}
+
+void		vao_add_vdata(GLuint vao, GLuint vbo, GLint size, GLboolean norm)
+{
+	static	GLint	index;
+
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(index++, size, GL_FLOAT, norm, 0, 0);
+}
--- OpenGl/vbo.c
+++ OpenGl/vbo.c
@@ -6,13 +6,28 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 19:49:04 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/21 19:57:13 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/03/22 16:43:04 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-GLuint	vbo(void)
+GLuint	vbo(GLenum target, GLsizeiptr size, GLvoid *data, GLenum usage)
 {
-	return (glGenBuffers());
+	GLuint		id;
+
+	id = glGenBuffers();
+	glBindBuffer(target, id);
+	glBufferData(target, size, (const GLvoid*)data, usage);
+	return (id);
+}
+
+GLuint	index_buffer(GLvoid *data, GLsizeiptr size)
+{
+	return (vbo(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
+
+GLuint	data_buffer(GLvoid *data, GLsizeiptr size)
+{
+	return (vbo(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
 }
