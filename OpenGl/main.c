9bc9ad7bdff21a91b3d60c580cfdde2f145f8b93
Frederic NIETO <fnieto@e3r8p15.42.fr>
2016-03-23 12:54:31 +0100

    removed useless code in main

--- OpenGl/main.c
+++ OpenGl/main.c
@@ -6,33 +6,11 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 19:38:41 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/22 21:02:42 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/03/23 12:53:30 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
-
-/*static t_params	*init_params(int ac, char **av)
-{
-	static t_params		a = {{512, 512}};
-	int					i;
-
-	if (ac <= 0)
-		return (&a);
-	i = -1;
-	while (++i < ac)
-		if (!ft_strcmp(av[i], "-res"))
-			if (ac - i - 1 < 2)
-				err("-res: not enough parameters\n");
-			else
-			{
-				a.res[0] = ft_atoi(av[++i]);
-				a.res[1] = ft_atoi(av[++i]);
-				if (a.res[0] < 1 || a.res[1] < 1)
-					err("-res: only res > 1 allowed\n");
-			}
-	return (&a);
-}*/
 
 int				main(void)
 {
