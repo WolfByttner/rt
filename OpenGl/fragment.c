c6db2bdad341b3f1a821a8aeffd31bc42aad87f3
Wicpar <frederic.artus.nieto@gmail.com>
2016-03-28 02:05:44 +0200

    4Kfps for 1 object :D (implemented timer for fps, more uniforms and actual geometry, needs rendering and lighting)

--- OpenGl/fragment.c
+++ OpenGl/fragment.c
@@ -6,31 +6,124 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/26 21:44:17 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/26 21:59:48 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/03/28 01:37:55 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #version 330 core
 
-typedef struct	s_ray
-{
-	vec4		ray;
-	vec4		material;
-}				t_ray;
+# define	PI			3.1415926535
 
-uniform ivec2 iResolution;
+# define	IGNORE		0
+# define	SPHERE		1
+# define	ELLYPSE		2
+# define	CONE		3
+# define	CYLINDER	4
+# define	BOX			5
+# define	PLANE		6
+# define	KLEIN		7
+# define	MOBIUS		8
+
+struct			s_cam
+{
+	vec3		pos;
+	vec3		ray;
+};
+
+struct			s_mat
+{
+	vec4		color;
+	float		metallic;
+	float		smoothness;
+	vec2		opacity;
+};
+
+struct			s_geo
+{
+	int			type;
+	vec3		pos;
+	float		bounds;
+	vec4		a;
+	vec4		b;
+	vec4		c;
+	vec4		d;
+	vec4		e;
+	s_mat		mat;
+};
+
+struct			s_res
+{
+	float		dst;
+	vec3		normal;
+	s_cam		cam;
+	s_mat		mat;
+};
+
+uniform ivec2 iResolution = ivec2(1, 1);
+uniform vec3 iCameraPosition = vec3(0, 0, 0);
+uniform vec2 iCameraRotation = vec2(0, 0);
+uniform float iCameraZoom = 1;
+uniform float iGlobalTime = 0;
 
 layout (location = 0) out vec4 outcol;
 
-t_ray	circle_intersect()
+vec3		sphere_norm(s_cam cam, s_res ret, s_geo object)
 {
-
+	return (-normalize(object.pos - (cam.pos + cam.ray * ret.dst)));
 }
 
-void	main()
+s_res		sphere_dst(s_cam cam, s_geo sp, s_res prev)
 {
-	vec2 uv = gl_FragCoord.xy / iResolution;
-	outcol = vec4(uv, 0, 1);
-	if (length (uv) > 1)
-		outcol.xy = vec2(0);
+	s_res		ret;
+	vec3		rc;
+	float		b, d, t;
+
+	rc = cam.pos - sp.pos;
+	b = dot(cam.ray, rc);
+	d = pow(b, 2) - dot(rc, rc) + pow(sp.bounds, 2);
+	t = -b - sqrt(abs(d));
+	ret.dst = mix(-1, t, step(0, min(t,d)));
+	if ((ret.dst > 0 && prev.dst <= 0) ||
+	(ret.dst > 0 && prev.dst > 0 && ret.dst < prev.dst))
+	{
+		ret.mat = sp.mat;
+		ret.normal = sphere_norm(cam, ret, sp);
+		return (ret);
+	}
+	return (prev);
+}
+
+/*
+** spherical to euclidian coordinates transformation
+*/
+
+vec3		make_view_vector(vec2 uv)
+{
+		return (normalize(
+			vec3(sin(uv.y) * cos(uv.x), sin(uv.y) * sin(uv.x), cos(uv.y))
+			).xzy);
+}
+
+void		main()
+{
+	s_cam	cam;
+	vec2	uv;
+	vec2	fov;
+
+	uv = -(gl_FragCoord.xy / iResolution - 0.5) *
+		iResolution.xy / float(iResolution.y) * iCameraZoom * PI / 2 -
+		iCameraRotation * PI + PI / 2;
+	cam.pos = iCameraPosition;
+	cam.ray = make_view_vector(uv);
+
+	s_geo geo;
+	geo.pos = vec3(5, 5, 10);
+	geo.bounds = 1;
+
+	s_res tmp;
+	tmp.dst = -1;
+	for (int i = 0; i < 300; ++i)
+		tmp = sphere_dst(cam, geo, tmp);
+
+	outcol = vec4(vec3(1/tmp.dst), 1);
 }
--- OpenGl/main.c
+++ OpenGl/main.c
@@ -6,14 +6,38 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 19:38:41 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/26 21:45:42 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/03/28 02:02:00 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
+#include <time.h>
+#include <stdio.h>
 #define FOO 1
 
-int				main(void)
+static GLFWwindow	*make_glfw(int width, int height)
+{
+	GLFWwindow			*window;
+
+	if (!glfwInit())
+		err("GLFW failed to init\n");
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	window = glfwCreateWindow(width, height, P_NAME, 0, 0);
+	if (!window)
+	{
+		glfwTerminate();
+		err("GLFW failed to create window");
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(0);
+	return (window);
+}
+
+int					main(void)
 {
 	GLFWwindow			*window;
 	int					i;
@@ -23,21 +47,7 @@ int				main(void)
 					FOO, FOO};
 	static const GLuint	inds[] = {0, 1, 3, 1, 2, 3};
 
-	if (!glfwInit())
-		err("GLFW failed to init\n");
-	glfwWindowHint(GLFW_RESIZABLE, 0);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow(1000, 1000, P_NAME, 0, 0);
-	if (!window)
-	{
-		glfwTerminate();
-		err("GLFW failed to create window");
-	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(0);
+	window = make_glfw(1000, 1000);
 	i = -1;
 	glClearColor(0, 0, 0, 1);
 	GLuint model = vao();
@@ -57,19 +67,30 @@ int				main(void)
 	shaders[1] = shader(GL_FRAGMENT_SHADER, 1, &(srcs[1]), &(sizes[1]));
 	GLuint program = shader_program(shaders, 2);
 	GLint ires = glGetUniformLocation(program, "iResolution");
+	GLint itime = glGetUniformLocation(program, "iGlobalTimeTime");
 	ft_putnbr(glGetError());
 	ft_putendl(" 1");
-
+	time_t t = time(0);
+	int t2;
+	int fps = 0;
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(program);
 		glUniform2i(ires, 1000, 1000);
+		glUniform1f(itime, (float)((double)(clock()) / .5e6));
 		glBindVertexArray(model);
 		glEnableVertexAttribArray(0);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		++fps;
+		if ((t2 = time(0)) > t)
+		{
+			printf("%i\n", fps);
+			t = t2;
+			fps = 0;
+		}
 	}
 	ft_putnbr(glGetError());
 	ft_putendl(" 2");
