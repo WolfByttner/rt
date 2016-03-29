9b6328bc7617ad6a085f9d6c4e06fd710321bbd3
Johan BYTTNER <jbyttner@student.42.fr>
2016-03-29 21:49:32 +0200

    Added space and shift keys to poll_keys.c

--- OpenGl/poll_keys.c
+++ OpenGl/poll_keys.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 20:50:54 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/29 21:23:25 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/03/29 21:48:28 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ static void	poll_movement_keys(GLFWwindow *window, float ftime, t_uniforms *u)
 	else if ((state = glfwGetKey(window, GLFW_KEY_S)) == GLFW_PRESS)
 		u->campos[0] -= u->cammov * ftime;
 	else if ((state = glfwGetKey(window, GLFW_KEY_A)) == GLFW_PRESS)
-		u->campos[1] += u->cammov * ftime;
+		u->campos[2] += u->cammov * ftime;
 	else if ((state = glfwGetKey(window, GLFW_KEY_D)) == GLFW_PRESS)
+		u->campos[2] -= u->cammov * ftime;
+	else if ((state = glfwGetKey(window, GLFW_KEY_SPACE)) == GLFW_PRESS)
+		u->campos[1] += u->cammov * ftime;
+	else if ((state = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)) == GLFW_PRESS)
 		u->campos[1] -= u->cammov * ftime;
 	else
 		return ;
