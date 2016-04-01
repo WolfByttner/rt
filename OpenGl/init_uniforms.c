89a642a182c7d0b12adc52b95e0071fddb97722e
Wicpar <frederic.artus.nieto@gmail.com>
2016-04-01 15:22:52 +0200

    increased mouse sensibility

--- OpenGl/init_uniforms.c
+++ OpenGl/init_uniforms.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 21:02:29 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/29 22:08:44 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/04/01 15:20:25 by fnieto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		init_uniforms(GLuint program)
 	u = get_uniforms();
 	ft_bzero(u, sizeof *u);
 	u->cammov = 10;
-	u->camsr = 0.0001;
+	u->camsr = 0.001;
 	u->icampos = glGetUniformLocation(program, "iCameraPosition");
 	u->icamrot = glGetUniformLocation(program, "iCameraRotation");
 	u->icamzoom = glGetUniformLocation(program, "iCameraZoom");
