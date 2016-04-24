4f788031cf9dffa5285a4bbea7e2dd38ae8fc419
Johan <jbyttner@42.fr>
2016-04-24 15:39:55 +0100

    Added null terminator to loadfile

--- OpenGl/load_file.c
+++ OpenGl/load_file.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/22 16:04:19 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/03/26 21:46:18 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/04/24 15:39:39 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,14 @@ void	load_file(char *file, GLchar **ptr, GLint *length)
 		err("Cannot open file!");
 	while ((r = read(fd, buf, BUFF_LENGTH)) > 0)
 	{
-		if (!(res = realloc(res, *length + r)))
+		if (!(res = realloc(res, *length + r + 1)))
 			err("No memory!");
 		memcpy(res + *length, buf, r);
 		*length += r;
 	}
 	if (r == -1)
 		err("Failed reading file!");
+	res[length] = '\0';
 	*ptr = res;
 	close(fd);
 }
