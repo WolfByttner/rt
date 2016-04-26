ac2c1fcdf7ef5f68e4c43f08114377372e1668d3
Madison DEKEN <mdeken@e2r4p16.42.fr>
2016-04-26 20:02:54 +0200

    shader_box done! Too many variables for the Norm

--- OpenGl/shader_box.c
+++ OpenGl/shader_box.c
@@ -3,18 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   shader_box.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: mdeken <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/24 13:50:39 by fnieto            #+#    #+#             */
-/*   Updated: 2016/04/24 13:51:29 by fnieto           ###   ########.fr       */
+/*   Created: 2016/04/26 19:33:43 by mdeken            #+#    #+#             */
+/*   Updated: 2016/04/26 19:52:50 by mdeken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-**TODO: the function
-*/
+ **sp.pos is the position of the point on the front/left/bottom face
+ **sp.a contains the lenght of each edge along the x, y and z axis
+ **Method: http://www.cs.cornell.edu/courses/Cs4620/2013fa/lectures/03raytracing1.pdf Ray-slab intersection chapter
+ */
+
+
+vec3		box_norm(float tin, vec3 tvin, vec3 tvout)
+{
+	vec3 norm;
+
+	norm = (tin == tvin.x) ? VEC3(1, 0, 0) : norm;
+	norm = (tin == tvin.y) ? VEC3(0, 1, 0) : norm;
+	norm = (tin == tvin.z) ? VEC3(0, 0, 1) : norm;
+	norm = (tin == tvout.x) ? VEC3(-1, 0, 0) : norm;
+	norm = (tin == tvout.y) ? VEC3(0, -1, 0) : norm;
+	norm = (tin == tvout.z) ? VEC3(0, 0, -1) : norm;
+	return (norm);
+}
 
 s_res		box_dst(s_geo sp, s_cam cam, s_res prev)
 {
+	float	tin, tout;
+	vec3	tmin, tmax, tvin, tvout;
+	s_res	ret;
+	vec3	inv_ray;
+
+	inv_ray = 1 / cam.ray;
+	tmin = (sp.pos - cam.pos) * inv_ray;
+	tmax = (sp.pos + sp.a.xyz - cam.pos) * inv_ray;
+	tvin = min(tmin, tmax);
+	tvout = max(tmin, tmax);
+	tin = max(max(tvin.x, tvin.y), tvin.z);
+	tout = (min(min(tvout.x, tvout.y), tvout.z));
+	if (tout < tin)
+		return (prev);
+	if (tin > 0 && (prev.dst <= 0 || tin < prev.dst))
+	{
+		ret.dst = tin;
+		ret.cam = cam;
+		ret.mat = sp.mat;
+		ret.normal = box_norm(tin, tvin, tvout);
+		return (ret);
+	}
 	return (prev);
 }
