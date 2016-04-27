d776514da83a9a91a45d420f0d6d1d65d1ad682b
Johan BYTTNER <jbyttner@student.42.fr>
2016-04-27 22:32:32 +0200

    Normed shader_box.c

--- OpenGl/shader_box.c
+++ OpenGl/shader_box.c
@@ -6,15 +6,17 @@
 /*   By: mdeken <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 19:33:43 by mdeken            #+#    #+#             */
-/*   Updated: 2016/04/27 21:19:52 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/04/27 22:32:09 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- **sp.pos is the position of the point on the front/left/bottom face
- **sp.a contains the lenght of each edge along the x, y and z axis
- **Method: http://www.cs.cornell.edu/courses/Cs4620/2013fa/lectures/03raytracing1.pdf Ray-slab intersection chapter
- */
+** sp.pos is the position of the point on the front/left/bottom face
+** sp.a contains the lenght of each edge along the x, y and z axis
+** Method:
+** http://www.cs.cornell.edu/courses/Cs4620/2013fa/lectures/03raytracing1.pdf
+** Ray-slab intersection chapter
+*/
 
 vec3		box_norm(float tin, vec3 tvin, vec3 tvout)
 {
