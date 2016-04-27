be25cb052a3814c9fbf877a471b9cf44ae28f6de
Johan BYTTNER <jbyttner@student.42.fr>
2016-04-27 21:55:22 +0200

    Normed main.c and added setup_program.c (some relocated code from main.c)

--- OpenGl/rt.h
+++ OpenGl/rt.h
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/21 19:50:48 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/04/27 18:19:12 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/04/27 21:49:54 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,27 @@ typedef	struct		s_properties
 {
 	int		width;
 	int		height;
+	GLuint	model;
+	GLuint	verts;
+	GLuint	indices;
+	GLuint	shaders[2];
+	GLuint	program;
+	GLint	ires;
+	GLint	itime;
 }					t_properties;
 
 void				err(char *msg);
 void				load_file(char *file, GLchar **ptr, GLint *length);
+GLuint				load_fragment(void);
 GLuint				shader(GLenum t, GLsizei c, GLchar **s, GLint *l);
 GLuint				shader_program(GLuint *shaders, size_t size);
 GLuint				vao(void);
+void				main_loop(GLFWwindow *window, t_properties *properties);
+GLFWwindow			*setup_program(t_properties *properties);
+void				poll_keys(GLFWwindow *window, float ftime);
 void				vao_add_indices(GLuint vao, GLuint vbo);
 void				vao_add_vdata(GLuint vao, GLuint vbo, GLint s, GLboolean n);
+t_properties		*get_properties(void);
 GLuint				vbo(GLenum t, GLsizeiptr s, GLvoid *d, GLenum u);
 GLuint				index_buffer(GLvoid *d, GLsizeiptr s);
 GLuint				data_buffer(GLvoid *d, GLsizeiptr s);
