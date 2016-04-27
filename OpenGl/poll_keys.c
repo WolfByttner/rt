38f672877925ff630b595695938b3399158a3c73
Johan BYTTNER <jbyttner@student.42.fr>
2016-04-27 22:30:23 +0200

    Normed poll_keys.c

--- OpenGl/poll_keys.c
+++ OpenGl/poll_keys.c
@@ -6,25 +6,25 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 20:50:54 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/04/09 21:42:19 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/04/27 22:29:41 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt_input.h"
 #include <math.h>
-#include <stdio.h>
 
 /*
 ** Rotation by hardcoded 3x3 rotation matrix
 */
 
-/* -- rot z --
+/*
+** -- rot z --
 ** 1, 			0,  	    0
 ** 0, 			w, 			-z
 ** 0, 			z, 			w
 ** -- rot y --
-** y,  		0, 			x 
-** 0,    		1, 			0, 
+** y,  		0, 			x
+** 0,    		1, 			0,
 ** x  		0, 			y
 ** -- rot res --
 ** y			0			x
@@ -48,12 +48,10 @@ static void	translate_key_move(t_uniforms *u, float move[3])
 	sins[3] = cos(a[1]);
 	u->campos[0] += sins[1] * move[0] + sins[0] * move[2];
 	u->campos[1] += sins[3] * move[1]
-		-sins[2] * move[2];
+		- sins[2] * move[2];
 	u->campos[2] += -sins[3] * sins[0] * move[0]
 		+ sins[2] * move[1]
 		+ sins[1] * sins[3] * move[2];
-	printf("move: |%f|%f|%f| degree: |%f|%f| res: |%f|%f|%f|\n", move[0], move[1], move[2],
-			a[0], a[1], u->campos[0], u->campos[1], u->campos[2]);
 }
 
 static void	poll_movement_keys(GLFWwindow *window, float ftime, t_uniforms *u)
@@ -61,7 +59,7 @@ static void	poll_movement_keys(GLFWwindow *window, float ftime, t_uniforms *u)
 	int		state;
 	float	move[3];
 
-	memset(move, 0, sizeof move);
+	ft_memset(move, 0, sizeof(move));
 	if ((state = glfwGetKey(window, GLFW_KEY_W)) == GLFW_PRESS)
 		move[2] = u->cammov * ftime;
 	if ((state = glfwGetKey(window, GLFW_KEY_S)) == GLFW_PRESS)
