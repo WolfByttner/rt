d84303c023bc461b0666429f9243b85c4aab7e6e
Johan BYTTNER <jbyttner@student.42.fr>
2016-04-29 19:44:40 +0200

    Reverting files for RTv1

--- OpenGl/shader_box.c
+++ OpenGl/shader_box.c
@@ -6,47 +6,17 @@
 /*   By: mdeken <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 19:33:43 by mdeken            #+#    #+#             */
-/*   Updated: 2016/04/29 17:21:09 by mdeken           ###   ########.fr       */
+/*   Updated: 2016/04/27 22:32:09 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- ** sp.pos is the position of the point on the front/left/bottom face
- ** sp.a contains the lenght of each edge along the x, y and z axis
- ** Method:
- ** http://www.cs.cornell.edu/courses/Cs4620/2013fa/lectures/03raytracing1.pdf
- ** Ray-slab intersection chapter
- */
-
-mat3		box_rot(vec4 angle)
-{
-	mat3	matx;
-	mat3	maty;
-	mat3	matz;
-
-//	mat4	matx;
-//	mat4	maty;
-//	mat4	matz;
-//	vec4	tmp;
-
-	matx[0] = VEC3(1, 0, 0);
-	matx[1] = VEC3(0, cos(angle.x), sin(angle.x));
-	matx[1] = VEC3(0, -sin(angle.x), cos(angle.x));
-
-	maty[0] = VEC3(cos(angle.y), 0, -sin(angle.y));
-	maty[1] = VEC3(0, 1, 0);
-	maty[2] = VEC3(sin(angle.y), 0, cos(angle.y));
-
-	matz[0] = VEC3(cos(angle.z), sin(angle.z), 0);
-	matz[1] = VEC3(-sin(angle.z), cos(angle.z), 0);
-	matz[2] = VEC3(0, 0, 1);
-
-	matx = inverse(matx);
-	maty = inverse(maty);
-	matz = inverse(matz);
-
-	return (matx * maty * matz);
-}
+** sp.pos is the position of the point on the front/left/bottom face
+** sp.a contains the lenght of each edge along the x, y and z axis
+** Method:
+** http://www.cs.cornell.edu/courses/Cs4620/2013fa/lectures/03raytracing1.pdf
+** Ray-slab intersection chapter
+*/
 
 vec3		box_norm(float tin, vec3 tvin, vec3 tvout)
 {
@@ -66,9 +36,9 @@ vec3		box_norm(float tin, vec3 tvin, vec3 tvout)
 }
 
 /*
- ** We do variable juggling to stay within the norm.
- ** ttmp is tmin and tvout is temporarily tmax.
- */
+** We do variable juggling to stay within the norm.
+** ttmp is tmin and tvout is temporarily tmax.
+*/
 
 s_res		box_dst(s_geo sp, s_cam cam, s_res prev)
 {
@@ -76,13 +46,7 @@ s_res		box_dst(s_geo sp, s_cam cam, s_res prev)
 	vec3	tvout;
 	vec3	ttmp;
 	s_res	ret;
-	mat3	tmp;
 
-//	sp.pos = sp.pos + (sp.a.xyz / 2);
-	tmp = box_rot(sp.b);
-//	sp.pos = tmp * sp.pos;
-//	ret.cam.ray = tmp * cam.ray;
-//	ret.cam.ray = normalize(ret.cam.ray);
 	ttmp = (sp.pos - cam.pos) / cam.ray;
 	tvout = (sp.pos + sp.a.xyz - cam.pos) / cam.ray;
 	tvin = min(ttmp, tvout);
@@ -92,10 +56,7 @@ s_res		box_dst(s_geo sp, s_cam cam, s_res prev)
 		return (prev);
 	if (ret.dst > 0 && (prev.dst <= 0 || ret.dst < prev.dst))
 	{
-		//ret.cam = cam;
-		//ret.cam.pos = tmp * cam.pos;
-		//ret.cam.ray = tmp * cam.ray;
-		//ret.cam.ray = normalize(ret.cam.ray);
+		ret.cam = cam;
 		ret.mat = sp.mat;
 		ret.normal = box_norm(ret.dst, tvin, tvout);
 		return (ret);
--- OpenGl/shader_mobius.c
+++ OpenGl/shader_mobius.c
@@ -6,7 +6,7 @@
 /*   By: fnieto <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/24 13:42:58 by fnieto            #+#    #+#             */
-/*   Updated: 2016/04/24 16:24:22 by fnieto           ###   ########.fr       */
+/*   Updated: 2016/04/29 19:44:09 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@
 ** c = (d.y * p ** 2 + 2 * p * d * p.y - 2 * p.z * (d.x * 2 * p.xy * d.xy)
 ** - p.y - 2 * d.z * (p.xy ** 2 + p.x))
 ** d = (p.y * p ** 2 - 2 * p.z * (p.xy ** 2 + p.x) - p.y)
-**
 */
 
 /*
@@ -34,53 +33,7 @@
 ** http://www.math.vanderbilt.edu/~schectex/courses/cubic/
 */
 
-s_res           mobius_dst(s_geo sp, s_cam cam, s_res prev)
+s_res			mobius_dst(s_geo sp, s_cam cam, s_res prev)
 {
-	float           a, b, c, d;
-	float           p, q, r;
-	float           rinner, router;
-	float           ddot, dxydot, dpdot, dpxydot, pdot, pxydot;
-	s_res           ret;
-
-	ddot = dot(cam.pos, cam.pos);
-	dxydot = dot(cam.pos.xy, cam.pos.xy);
-	dpxydot = dot(cam.pos.xy, cam.ray.xy);
-	dpdot = dot(cam.pos, cam.ray);
-	pdot = dot(cam.ray, cam.ray);
-	pxydot = dot(cam.ray.xy, cam.ray.xy);
-	a = ddot * cam.pos.y - 2 * dxydot * cam.pos.z;
-	b = ddot * cam.ray.y + 2 * (cam.pos.y * dpdot - cam.ray.z * dxydot
-			- cam.pos.x * cam.pos.z - 2 * dpxydot);
-	c = cam.pos.y * pdot + 2 * (dpdot * cam.ray.y
-			- cam.ray.z * (cam.pos.x * 2 * dpxydot)
-			- cam.pos.z * (pxydot + cam.ray.x))
-		- cam.ray.y;
-	d = cam.ray.y * pdot - 2 * cam.ray.z * (pxydot + cam.ray.x) - cam.ray.y;
-	p = -b / (3 * a);
-	q = pow(p, 3) + (b * c - 3 * a * d) / (6 * pow(a, 2));
-	r = c / (3 * a);
-	// TODO: Check if these are actually the correct tests
-	rinner = pow(q, 2) + pow((r - pow(b, 2)), 3);
-	if (rinner <= 0)
-		return (prev);
-	rinner = sqrt(rinner);
-	router = q - rinner;
-	if (router < 0)
-		return (prev);
-	router = pow(router, 1 / 3);
-	ret.dst = router;
-	router = q + rinner;
-	if (router < 0)
-		return (prev);
-	router = pow(router, 1 / 3);
-	ret.dst += router;
-	ret.dst += p;
-	if (ret.dst > 0 && (prev.dst <= 0 || ret.dst < prev.dst))
-	{
-		ret.mat = sp.mat;
-		ret.cam = cam;
-		ret.normal = vec3(0, 1, 0);
-		return (ret);
-	}
 	return (prev);
 }
