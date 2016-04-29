0758ed442f312d6227e0a13ec45061713ade9b2c
Madison DEKEN <mdeken@e3r8p21.42.fr>
2016-04-29 17:17:22 +0200

    norme window_size_callback

--- OpenGl/window_size_callback.c
+++ OpenGl/window_size_callback.c
@@ -6,13 +6,16 @@
 /*   By: mdeken <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 19:48:36 by mdeken            #+#    #+#             */
-/*   Updated: 2016/04/28 20:23:36 by mdeken           ###   ########.fr       */
+/*   Updated: 2016/04/29 17:16:48 by mdeken           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt_input.h"
 
-#include <stdio.h>
+/*
+** Whenever the window is resized window_size_callback is called, and saves
+** the new width and height of the window.
+*/
 
 void	window_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -22,5 +25,4 @@ void	window_size_callback(GLFWwindow *window, int width, int height)
 	properties = get_properties();
 	properties->width = width;
 	properties->height = height;
-	printf("%d %d\n", width, height);
 }
